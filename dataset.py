a685ef1fa99656055092ef719bd8739e28c68fef
Sean Williams <SWilliams17655@hotmail.com>
2023-04-14 09:17:34 -0400

    Adding html interface for Data Manager.

--- dataset.py
+++ dataset.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.style as style
+
 import numpy as np
+
 
 
 class AIDataset:
@@ -12,9 +14,7 @@ class AIDataset:
         self.y = pd.DataFrame()
 
     def load(self, os_loc=None, y_label=None):
-
-        """
-        Loads a data set for use in the machine learning program to test.
+        """ Loads a data set for use in the machine learning program to test.
 
         :param os_loc: [Optional] String representing the file to be loaded. If None, the function
         will request user provide a location via the command prompt. Default = None.
@@ -36,92 +36,28 @@ class AIDataset:
 
         return self.data_file
 
-    def show_table_in_html(self, filename):
-        """
-        Sends the dataset passed to an html file for viewing
-        :param filename: String representing the filename where the html will be sent.
-        """
-        new_table = self.x
-        new_table["Classifier"] = self.y
-        new_table.to_html(filename)
-
-    def show_data_description_in_html(self, filename):
-        """
-        Sends the dataset passed to an html file for viewing
-        :param filename: String representing the filename where the html will be sent.
-        """
-        new_table = self.x
-        new_table["Classifier"] = self.y
-        new_table.describe().to_html(filename)
-
-    def get_image_at(self, position=0):
-        """
-        Displays an image representing the character following processing.
-
-        :param position: Position of the image in the array to be plotted.
-        :return: 28x28 image representing the data at that location.
-        """
-
-        image = self.x.iloc[position]
-        image = image.to_numpy().reshape((28, 28))
-        return image
-
-    def normalize(self, label, min_value, max_value):
-        """
-        Displays an image representing the character following processing.
-
-        :param label: Label to be normalized.
-        :param min_value: minimum value possible for data to be normalized.
-        :param max_value: maximum value possible for data to be normalized.
-        """
-        self.x[str(label + "_norm")] = self.x[label].apply(lambda x: (x - min_value) / max_value)
-
-    def show_hist(self, attribute, min_range=0, max_range=1, step=.25, y_axis_top=.75):
+    def show_histogram(self):
         """
         Plots multiple scatter plots showing relationship between attributes in
         attributes array. If mask is a value other than none, graph only shows correlating data for that value of Y.
-
-        :param attribute: An array of strings with the title of the attributes to be correlated.
-        :param min_range: Minimum range of the histogram x-axis.
-        :param max_range: Maximum range of the histogram x-axis.
-        :param step: Step used along the x-axis.
-        :param y_axis_top: Top of the y-axis.
         """
         style.use('ggplot')
-        # 'Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic',
-        # 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn-v0_8', 'seaborn-v0_8-bright',
-        # 'seaborn-v0_8-colorblind', 'seaborn-v0_8-dark', 'seaborn-v0_8-dark-palette', 'seaborn-v0_8-darkgrid',
-        # 'seaborn-v0_8-deep', 'seaborn-v0_8-muted', 'seaborn-v0_8-notebook', 'seaborn-v0_8-paper',
-        # seaborn-v0_8-pastel', 'seaborn-v0_8-poster', 'seaborn-v0_8-talk', 'seaborn-v0_8-ticks',
-        # 'seaborn-v0_8-white', 'seaborn-v0_8-whitegrid', 'tableau-colorblind10'
+        for attribute in self.x.columns:
+            fig, (ax1) = plt.subplots(1, 1, figsize=(20, 9))
+            true_array = self.x.where((self.y == 1))
+            false_array = self.x.where((self.y == 0))
 
-        bin_range = np.arange(min_range, max_range, step)
+            ax1.hist([true_array[attribute], false_array[attribute]], bins=10, density=False,
+                     stacked=False, histtype='bar', rwidth=0.7, color=["royalblue", "tomato"],
+                     label=["True", "False"])
 
-        fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 9))
-        true_array = self.x.where((self.y == 1))
-        false_array = self.x.where((self.y == 0))
-        ax1.hist(true_array[attribute], bins=bin_range, density=True,
-                 histtype='bar', rwidth=0.7, color="cadetblue",
-                 label=attribute)
-        ax2.hist(false_array[attribute], bins=bin_range, density=True,
-                 histtype='bar', rwidth=0.7, color="darkkhaki",
-                 label=attribute)
+            ax1.set_ylabel("Density of Dataset")
+            ax1.set_title(attribute)
+            ax1.legend()
 
-        ax1.set_ylabel("Density of Dataset")
-        ax1.set_title("Classifier is True")
-        ax1.legend()
-        ax1.set_xticks(bin_range)
-        ax1.set_ylim(top=y_axis_top)
-        ax2.set_xlabel("Values")
-        ax2.set_ylabel("Density of Dataset")
-        ax2.set_title("Classifier is False")
-        ax2.set_xticks(bin_range)
-        ax2.set_ylim(top=y_axis_top)
-        ax2.legend()
+            plt.show()
 
-        plt.show()
-
-    def show_corr_matrix(self):
+    def show_correlation_matrix(self):
         """
         Plots a correlation matrix
         """
@@ -132,38 +68,49 @@ class AIDataset:
         print(corr_matrix["Classifier"].sort_values(ascending=False))
         print("\n")
 
-        fig, (ax1) = plt.subplots(1, 1, figsize=(9, 9))
+        fig, (ax1) = plt.subplots(1, 1, figsize=(20, 9))
         color_axes = ax1.matshow(corr_matrix, cmap='Greens')
         fig.colorbar(color_axes)
         ax1.set_xticks(np.arange(len(corr_matrix)), corr_matrix.columns, rotation=90)
         ax1.set_yticks(np.arange(len(corr_matrix)), corr_matrix.columns)
+        ax1.set_title("Correlation Matrix")
         plt.show()
 
-    def show_scatter(self, attribute1, attribute2):
+    def show_scatter_plot(self):
         """
         Plots a scatter plot relationship between three variables with x being attribute1, y is attribute 2, and
         y classifier is color of information.
 
-        :param attribute1: Variable along the x-axis.
-        :param attribute2: Variable along the y-axis.
-
         """
+        for attribute1 in self.x.columns:
+            for attribute2 in self.x.columns:
+                if attribute1 != attribute2:
+                    style.use('ggplot')
 
-        style.use('ggplot')
+                    print(plt.style.available)
 
-        print(plt.style.available)
+                    fig, (ax1) = plt.subplots(1, 1, figsize=(10, 9))
+                    true_array = self.x.where((self.y == 1))
+                    false_array = self.x.where((self.y == 0))
 
-        fig, (ax1) = plt.subplots(1, 1, figsize=(10, 9))
-        true_array = self.x.where((self.y == 1))
-        false_array = self.x.where((self.y == 0))
+                    ax1.scatter(true_array[attribute1], self.x[attribute2], color="royalblue", label="Classifier: True")
+                    ax1.scatter(false_array[attribute1], self.x[attribute2], color="tomato", label="Classifier: False")
+                    ax1.set_title("Compare " + attribute1 + " & " + attribute2)
+                    ax1.legend()
+                    ax1.set_xlabel(attribute1)
+                    ax1.set_ylabel(attribute2)
+                    plt.show()
 
-        ax1.scatter(true_array[attribute1], self.x[attribute2], color="cadetblue", label="Classifier: True")
-        ax1.scatter(false_array[attribute1], self.x[attribute2], color="darkkhaki", label="Classifier: False")
-        ax1.set_title("Compare: Classifier, " + attribute1 + ", & " + attribute2)
-        ax1.legend()
-        ax1.set_xlabel(attribute1)
-        ax1.set_ylabel(attribute2)
-        plt.show()
+    def normalize(self, attribute):
+        """
+        Displays an image representing the character following processing.
+
+        :param attribute: Label to be normalized.
+        """
+        min_value = self.x[attribute].min()
+        max_value = self.x[attribute].max()
+        print(f"Normalizing with min value of {min_value} and max value of {max_value}")
+        self.x[str(attribute + "_norm")] = self.x[attribute].apply(lambda x: (x - min_value) / max_value)
 
     def remove_nulls(self, attribute):
         """
--- main.py
+++ main.py
@@ -1,46 +1,16 @@
 import ai_toolbox
 from dataset import AIDataset
 import numpy as np
+from flask import Flask, flash, render_template, request
+
+app = Flask(__name__)
 
 train_set = AIDataset()
 train_set.load(os_loc="Data/train.csv", y_label='target')
-print(train_set.data_file)
-#
-# train_set.remove_zeros("SkinThickness")
-# train_set.remove_zeros("Insulin")
-# train_set.normalize("Pregnancies", 0, 17)
-# train_set.normalize("Glucose", 0, 200)
-# train_set.normalize("BloodPressure", 0, 122)
-# train_set.normalize("SkinThickness", 0, 99)
-# train_set.normalize("Insulin", 0, 846)
-# train_set.normalize("BMI", 0, 70)
-# train_set.normalize("Age", 0, 90)
-#
-# eval_set = AIDataset("Data/eval.csv")
-# eval_set.load(y_label='Label')
-# eval_set.remove_zeros("SkinThickness")
-# eval_set.remove_zeros("Insulin")
-# eval_set.normalize("Pregnancies", 0, 17)
-# eval_set.normalize("Glucose", 0, 200)
-# eval_set.normalize("BloodPressure", 0, 122)
-# eval_set.normalize("SkinThickness", 0, 99)
-# eval_set.normalize("Insulin", 0, 846)
-# eval_set.normalize("BMI", 0, 70)
-# eval_set.normalize("Age", 0, 90)
-#
-# train_set.show_table_in_html("HTML/Table.html")
-# train_set.show_data_description_in_html("HTML/Describe.html")
-#
-# attributes=["Pregnancies_norm", "Glucose_norm",	"BloodPressure_norm", "SkinThickness_norm",
-#             "Insulin_norm",	"BMI_norm",	"Age_norm"]
-#
-# #model1 = ai_toolbox.train_random_forest(train_set.x[attributes], train_set.y, eval_set.x[attributes], eval_set.y)
-# #model2 = ai_toolbox.train_sgd(train_set.x[attributes], train_set.y, eval_set.x[attributes], eval_set.y)
-# #model3 = ai_toolbox.train_svm(train_set.x[attributes], train_set.y, eval_set.x[attributes], eval_set.y)
-# model4 = ai_toolbox.train_neural_network(train_set.x[attributes], train_set.y, eval_set.x[attributes], eval_set.y)
-#
-# # ai_toolbox.save_model(model1, "file.pkl")
-# # model5 = ai_toolbox.load_model("file.pkl")
-# pred=ai_toolbox.make_one_prediction(model4, eval_set.x.iloc[100][attributes])
-# print(pred)
-# print(eval_set.y.iloc[100])
+
+@app.route("/")
+def home_page():
+    return render_template("DataNannyHome.html")
+
+if __name__ == "__main__":  # is the same thing as calling the run function
+    app.run(host="0.0.0.0")
